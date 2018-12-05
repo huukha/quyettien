e48909cbdf4c3dcb8b07b43d529eef590dc6d94c
Kha <hk0.2582670367@gmail.com>
2018-12-05 09:49:11 +0700

    add unit test

---
+++ QuyetTien.Tests/Controllers/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuyetTien.Controllers;
+using System.Web.Mvc;
+using QuyetTien.Models;
+using System.Collections.Generic;
+
+namespace QuyetTien.Tests.Controllers
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestIndex()
+        {
+            var controller = new CreateController();
+            var result = controller.Index() as ViewResult;
+            var db = new CS4PEEntities();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Model, typeof(List<BangSanPham>));
+            Assert.AreEqual(db.BangSanPhams.Count(), ((List<BangSanPham>)result.Model).Count);
+        }
+    }
+}
