92a8858b311ec105db9d8a192e697c9ab4c2f5d1
Kha <hk0.2582670367@gmail.com>
2018-12-12 08:43:39 +0700

    test details

--- QuyetTien.Tests/Controllers/UnitTest1.cs
+++ QuyetTien.Tests/Controllers/UnitTest1.cs
@@ -5,6 +5,11 @@ using QuyetTien.Controllers;
 using System.Web.Mvc;
 using QuyetTien.Models;
 using System.Collections.Generic;
+using System.Web;
+using System.Web.Routing;
+using Moq;
+using System.IO;
+
 
 namespace QuyetTien.Tests.Controllers
 {
@@ -28,11 +33,26 @@ namespace QuyetTien.Tests.Controllers
         {
             var controller = new CreateController();
             var result = controller.Create() as ViewResult;
-            var db = new CS4PEEntities();
 
             Assert.IsNotNull(result);
             Assert.IsNotNull(result.ViewData["Loai_id"]);
             Assert.IsInstanceOfType(result.ViewData["Loai_id"], typeof(SelectList));
+        }
+
+        [TestMethod]
+        public void TestDetails()
+        {
+            var controller = new CreateController();
+            var context = new Mock<HttpContextBase>();
+            context.Setup(c => c.Server.MapPath("~/App_Data")).Returns("~/App_Data");
+            controller.ControllerContext = new ControllerContext(context.Object, new RouteData(), controller);
+
+            var result = controller.Details("1") as FilePathResult;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("images", result.ContentType);
+            var path = Path.Combine("~/App_Data", "1");
+            Assert.AreEqual(path, result.FileName);
         }
     }
 }
